26d35294a5e2386407ca7bc5f83e6d563c57eb9a
Luki <muyimacha@gmail.com>
2022-05-01 19:29:58 -0300

    first commit

---
+++ script.py
@@ -0,0 +1,9 @@
+#import numpy as np
+#import pandas as pd 
+#import gspread
+#import gspread_dataframe
+
+# from oauth2client.service_account import ServiceAccountCredentials
+
+from telegram.ext import Updater
+updater = Updater(token='TOKEN', use_context=True)
